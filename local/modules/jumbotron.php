6055aa9eaad7feb6543b8e4d6122930d3087203d
Stefan Haack <shaack@gmail.com>
2018-11-06 16:33:01 +0100

    default modules

---
+++ local/modules/jumbotron.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/reboot
+ * License: MIT, see file 'LICENSE'
+ */
+
+/**  @var \Shaack\Reboot\Slice $slice */
+?>
+<div class="jumbotron">
+    <h1 class="display-4"><?php $slice->config("headline") ?></h1>
+    <p class="lead"><?php $slice->config("lead") ?></p>
+    <hr class="my-4">
+    <?php $slice->content() ?>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button"><?php $slice->config("buttonText") ?></a>
+    </p>
+</div>
