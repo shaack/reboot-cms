ed30fdb9a3264a62d8f02a08993d3f1cbf7a0635
Stefan Haack <shaack@gmail.com>
2018-11-07 02:15:55 +0100

    standard markdown module

---
+++ local/modules/standard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/reboot
+ * License: MIT, see file 'LICENSE'
+ */
+?>
+
+<div class="container">
+    <?php $this->render("content", true) ?>
+</div>
