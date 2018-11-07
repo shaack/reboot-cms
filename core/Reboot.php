5263ef4bdeadbd808d81ac7212bd8582bbbb1e7a
Stefan Haack <shaack@gmail.com>
2018-11-07 02:15:06 +0100

    sets route and renders page

--- core/Reboot.php
+++ core/Reboot.php
@@ -12,14 +12,24 @@ require 'Page.php';
 
 class Reboot
 {
-    private $config;
-    private $route;
+    public $config;
+    public $route;
+    public $parsedown;
 
-    public function __construct()
+    public function __construct($uri)
     {
-        // $this->config = Yaml::parseFile(__DIR__ . '/../local/config.yml');
-        $this->route = rtrim($_SERVER['REQUEST_URI'], "/");
+        $configJson = file_get_contents(__DIR__ . '/../local/config.json');
+        $this->config = json_decode($configJson);
+        $this->route = rtrim($uri, "/");
+        $this->parsedown = new \Parsedown();
+        if(!$this->route || is_dir(__DIR__ . '/../local/pages' . $this->route)) {
+            $this->route = $this->route . "/index";
+        }
+    }
+
+    public function render()
+    {
         $page = new Page($this);
-        $page->render();
+        $page->render($this->route);
     }
 }
