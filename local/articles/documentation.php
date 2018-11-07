b869dbfd9584a7f4f329742855283ad6fe1e4428
Stefan Haack <shaack@gmail.com>
2018-11-07 21:07:56 +0100

    documentation block includes README.md

--- local/articles/documentation.php
+++ local/articles/documentation.php
@@ -1,2 +1,8 @@
 <?php
 
+use Shaack\Reboot\Block;
+
+global $reboot;
+$content = file_get_contents($reboot->baseDir . "/README.md");
+$block = new Block("markdown", $content);
+echo($block->render());
--- local/articles/helloworld.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Author and copyright: Stefan Haack (https://shaack.com)
- * Repository: https://github.com/shaack/reboot-cms
- * License: MIT, see file 'LICENSE'
- */
-?>
-Hello World!
-
