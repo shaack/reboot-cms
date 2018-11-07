cf532a26625c3d40dc29f102a03d31bb71005d16
Stefan Haack <shaack@gmail.com>
2018-11-07 03:17:14 +0100

    added jquery as composer require

--- local/templates/default.php
+++ local/templates/default.php
@@ -11,24 +11,9 @@
     <link href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/local/assets/style/default.css" rel="stylesheet">
 </head>
-
 <body>
-
-<main role="main" class="container">
-
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-    </div>
-
-</main>
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="//assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-<!-- <script src="../../assets/js/vendor/popper.min.js"></script> -->
+<?php $this->renderArticle() ?>
+<script src="/vendor/components/jquery/jquery.slim.js"></script>
 <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
