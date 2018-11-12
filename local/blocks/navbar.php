9e50a8b678512704a22bd6d1d0b8935a596955c9
Stefan Haack <shaack@gmail.com>
2018-11-12 10:08:18 +0100

    bugfix navbar

--- local/blocks/navbar.php
+++ local/blocks/navbar.php
@@ -11,12 +11,12 @@ $config = $reboot->config['navbar'];
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="/"><?php echo $config["brand"] ?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav mr-auto">
             <?php
             $structure = $config['structure'];
