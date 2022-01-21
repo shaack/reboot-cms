0bce42069dacb0c2b0eaceb06f5b729daa07ead3
shaack <shaack@gmail.com>
2022-01-21 15:25:12 +0100

    switched to BS 5 via CDN

--- site/template.php
+++ site/template.php
@@ -8,51 +8,55 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <base href="<?= $site->getWebPath() ?>/" />
+    <base href="<?= $site->getWebPath() ?>/"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <title>Reboot CMS</title>
-    <link href="../web/admin/vendor/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link href="../web/assets/styles/screen.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./assets/styles/screen.css" rel="stylesheet">
 </head>
 <body>
 <?php $navbarConfig = $site->getConfig()['navbar']; ?>
 <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
-    <a class="navbar-brand" href="<?= $site->getWebPath() ?>/"><?php echo $navbarConfig["brand"] ?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
-            aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav mr-auto">
-            <?php
-            $structure = $navbarConfig['structure'];
-            if ($structure) {
-                foreach ($structure as $label => $path) {
-                    $active = false;
-                    if($path === "/") {
-                        $active = $request->getPath() === $path;
-                    } else {
-                        $active = substr($request->getPath(), 0, strlen($path)) === $path;
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?= $site->getWebPath() ?>/"><?php echo $navbarConfig["brand"] ?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+                <?php
+                $structure = $navbarConfig['structure'];
+                if ($structure) {
+                    foreach ($structure as $label => $path) {
+                        $active = false;
+                        if ($path === "/") {
+                            $active = $request->getPath() === $path;
+                        } else {
+                            $active = substr($request->getPath(), 0, strlen($path)) === $path;
+                        }
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link <?= $active ? "active" : "" ?>" href="<?= $site->getWebPath() . $path ?>"><?= $label ?></a>
+                        </li>
+                        <?php
                     }
-                    ?>
-                    <li class="nav-item <?=  $active ? "active" : "" ?>">
-                        <a class="nav-link" href="<?= $site->getWebPath() . $path ?>"><?= $label ?></a>
-                    </li>
-                    <?php
                 }
-            }
-            ?>
-        </ul>
+                ?>
+            </ul>
+        </div>
     </div>
 </nav>
 <?php
 echo($page->render($request));
 ?>
-<script src="../web/admin/vendor/jquery/jquery.min.js"></script>
-<script src="../web/admin/vendor/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
 </body>
 </html>
