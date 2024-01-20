6eaaba5f5d860f8abdb8e97b02ec893b2737f1c1
shaack <shaack@gmail.com>
2024-01-20 12:56:03 +0100

    Login screen optimized

--- core/admin/pages/login.php
+++ core/admin/pages/login.php
@@ -10,7 +10,7 @@ $authentication = $site->getAddOn("Authentication");
 $username = htmlentities(@$_REQUEST["username"]);
 $password = htmlentities(@$_REQUEST["password"]);
 $error = null;
-if($username) {
+if ($username) {
     if ($authentication->login($username, $password)) {
         Logger::info("Login success " . $username);
         $reboot->redirect($reboot->getBaseWebPath() . $site->getWebPath() . "/pages");
@@ -24,7 +24,7 @@ if($username) {
 <div class="container">
     <div class="card mx-auto" style="max-width: 32rem; margin-top: 200px;">
         <div class="card-body">
-            <?php if($error) { ?>
+            <?php if ($error) { ?>
                 <div class="alert alert-danger">
                     <?= $error ?>
                 </div>
@@ -32,7 +32,7 @@ if($username) {
             <form id="loginForm" class="center-horizontal form-md" method="post">
                 <fieldset>
                     <!-- <legend>Login</legend> -->
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="username" class="sr-only">Username</label>
                         <input id="username" name="username" class="form-control form-control-pair-top mb-1"
                                placeholder="Username" required="required" type="text" value="<?= $username ?>"
@@ -40,9 +40,11 @@ if($username) {
                         <label for="password" class="sr-only">Password</label>
                         <input id="password" name="password" class="form-control form-control-pair-bottom"
                                placeholder="Password"
-                               type="password" required="required" value=""></div>
-                    <input id="check" name="check" type="hidden" value=""></fieldset>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                               type="password" required="required" value="">
+                    </div>
+                    <input id="check" name="check" type="hidden" value="">
+                </fieldset>
+                <button class="btn btn-primary btn-block px-4" type="submit">Login</button>
             </form>
         </div>
     </div>
